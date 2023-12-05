47bf9a409e5c3684ac4c932bc08a689a905eb283
zaneliu <ezreeeeal@gmail.com>
2023-12-05 14:51:57 +0800

    chore: 构建环境变更

--- shims.d.ts
+++ shims.d.ts
@@ -1,11 +1,28 @@
-
-
 declare namespace astroHTML.JSX {
   interface DOMAttributes {
-    "th:href"?: string
-    "th:each"?: string
-    "th:text"?: string
-    "th:if"?: string
-    "th:utext"?: string
+    // 基本的 Thymeleaf 属性
+    "th:text"?: string;
+    "th:utext"?: string;
+    "th:href"?: string;
+    "th:src"?: string;
+    "th:each"?: string;
+    "th:if"?: string;
+    "th:unless"?: string;
+    "th:switch"?: string;
+    "th:case"?: string;
+    "th:fragment"?: string;
+    "th:object"?: string;
+    "th:with"?: string;
+    "th:classappend"?: string;
+    "th:id"?: string;
+    "th:name"?: string;
+    "th:value"?: string;
+    "th:attr"?: string;
+    "th:attrprepend"?: string;
+    "th:attrappend"?: string;
+    "th:style"?: string;
+    "th:onclick"?: string;
+    "th:onsubmit"?: string;
+    "th:onchange"?: string;
   }
 }
