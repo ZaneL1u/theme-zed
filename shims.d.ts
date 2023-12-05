ee6afdf436d10741eed55806104a0166e4a121a2
zaneliu <ezreeeeal@gmail.com>
2023-12-05 11:03:24 +0800

    feat(setting): 更新设置

---
+++ shims.d.ts
@@ -0,0 +1,11 @@
+
+
+declare namespace astroHTML.JSX {
+  interface DOMAttributes {
+    "th:href"?: string
+    "th:each"?: string
+    "th:text"?: string
+    "th:if"?: string
+    "th:utext"?: string
+  }
+}
