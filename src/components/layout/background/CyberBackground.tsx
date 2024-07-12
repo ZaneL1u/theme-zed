0a01cf5f3601f9f5539ea911b05b7f555debf3b6
zaneliu <hi@zaneliu.me>
2024-07-12 18:35:00 +0800

    chore: 注释

--- src/components/layout/background/CyberBackground.tsx
+++ src/components/layout/background/CyberBackground.tsx
@@ -2,9 +2,12 @@ import { createSignal } from 'solid-js'
 import { drawBg } from './CyberBackground'
 
 export function CyberBackground() {
-  const wrapper = createSignal<HTMLElement | null>(null)
+  let context: HTMLCanvasElement
+  let wrapper: HTMLDivElement
 
-  const context = createSignal<HTMLCanvasElement | null>(null)
+  // const wrapper = createSignal<HTMLElement | null>(null)
+
+  // const context = createSignal<HTMLCanvasElement | null>(null)
 
   // onMounted(() => {
   //   nextTick(() => {
@@ -18,7 +21,42 @@ export function CyberBackground() {
 
   return (
     <>
-      <div>123123</div>
+      <div ref={wrapper} />
+      <canvas
+      // v-if="isMounted"
+        ref={context}
+      />
     </>
   )
 }
+
+// <script lang="ts" setup>
+// import { drawBg } from "./CyberBackground";
+// const wrapper = ref<HTMLElement | null>(null);
+
+// const context = ref<HTMLCanvasElement | null>(null);
+
+// onMounted(() => {
+//   nextTick(() => {
+//     if (wrapper.value && context.value) {
+//       drawBg(context.value);
+//     }
+//   });
+// });
+
+// const isMounted = useMounted();
+// </script>
+
+// <template>
+//   <div
+//     class="fixed top-0 bottom-0 left-0 right-0 pointer-events-none print:hidden z--1"
+//     ref="wrapper"
+//   >
+//     <canvas
+//       v-if="isMounted"
+//       ref="context"
+//       :width="wrapper.clientWidth"
+//       :height="wrapper.clientHeight"
+//     ></canvas>
+//   </div>
+// </template>
